5d4890bbf7b3e75eb1461730146fbb54d10887bd
Strasseneck <121754696+Strasseneck@users.noreply.github.com>
2023-12-01 11:43:01 +0100

    Add get bookID via ISBN search function

--- homelessbooks/books/static/books/script.js
+++ homelessbooks/books/static/books/script.js
@@ -2,11 +2,42 @@
 
 // Get button click
 $('#isbn-submit').on('click', function () {
-    let isbn = $('#isbn-search').value();
-    getBookInfor(isbn);
+    console.log("isbn search clicked");
+    let isbn = $('#isbn-search').val();
+    getBookId(isbn);
 })
 
+// Get google volume id via ISBN
+async function getBookDataIsbn(isbn) {
+    console.log("function called");
+    console.log(`ISBN: ${isbn}`);
 
-function getBookInfo(isbn) {
+    try {
+        const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`);
+    
+        if(!response.ok) {
+            throw new Error(`HTTP error! Status ${response.status}`)
+        }
 
+        const result = await response.json();
+        return result;
+        }
+        catch(error) {
+            console.error("Error getting book data:", error);
+            return null;
+        }
 }
+
+// Get bookID function
+async function getBookId(isbn) {
+    let bookId;
+    const result = await getBookDataIsbn(isbn);
+
+    if(result !== null) {
+        bookId = result.items[0].id;
+    }
+    else {
+        console.log("Search returned no book data")
+    }
+}
+
