0544090ab00b34d55fa0b487eac133416d49331e
Strasseneck <121754696+Strasseneck@users.noreply.github.com>
2023-12-05 11:35:41 +0100

    Add Book and Book Image models

--- homelessbooks/books/models.py
+++ homelessbooks/books/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=750)
+    subtitle = models.CharField(max_length=750)
+    authors = models.CharField(max_length=750)
+    publisher = models.CharField(max_length=750)
+    category = models.CharField(max_length=100)
+    published_date = models.CharField(max_length=8)
+    page_count = models.IntegerField(validators=[MinValueValidator(1)])
+    height = models.DecimalField(max_digits=5, decimal_places=2)
+    width = models.DecimalField(max_digits=5, decimal_places=2)
+    thickness = models.DecimalField(max_digits=5, decimal_places=2)
+    print_type = models.CharField(max_length=10)
+    dust_jacket = models.CharField(max_length=10)
+    description = models.TextField()
+    binding = models.CharField(max_length=10)
+    condition = models.CharField(max_length=10)
+    images = models.ManyToManyField('BookImage', blank=True, related_name='photos')
+
+class BookImage(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='book_images/')
+
+
+
