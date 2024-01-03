9ceae4198e51d84e60239101017259ac263e32ae
Strasseneck <121754696+Strasseneck@users.noreply.github.com>
2024-01-03 18:33:52 +0100

    Add model changes etc

--- homelessbooks/books/admin.py
+++ homelessbooks/books/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Book, BookImage
+from .models import Author, Book, BookImage
 
 class imageAdmin(admin.ModelAdmin):
     list_display = ["book", "image"]
 
-# Register your models here.
+# Register your models here
+admin.site.register(Author)
 admin.site.register(Book)
 admin.site.register(BookImage)
